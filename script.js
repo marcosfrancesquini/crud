d9f3cc7cb30ebfd4408b8c0dde4993c2a9140309
marcosfrancesquini <francesquini.personal@gmail.com>
2025-03-03 16:39:37 -0300

    javascript inserir elementos

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("ok");
+listaProdutos = []
+
+const btnInserir = document.getElementById("inserir")
+
+btnInserir.addEventListener('click', function (event) {
+    event.preventDefault();
+    const produto    = document.getElementById('produtoNome').value;
+    const categoria  = document.getElementById('produtoCategoria').value;
+    const quantidade = document.getElementById('produtoQuantidade').value;
+
+    listaProdutos.push( {
+        produto: produto,
+        categoria: categoria,
+        quantidade: quantidade
+    })
+
+   // console.log(listaProdutos)
+    let htmlContent = '';
+    const elementoListaProdutos = document.getElementById("listaProdutos");
+
+    listaProdutos.forEach(produto => {
+        htmlContent += `<div>${produto.produto}, ${produto.categoria}, ${produto.quantidade}</div>`;
+    });
+
+    elementoListaProdutos.innerHTML = htmlContent;
+    
+
+});
