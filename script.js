3bc156b3de74dcfbcc498a9863c768d768785cab
marcosfrancesquini <francesquini.personal@gmail.com>
2025-03-04 09:11:49 -0300

    nome produto obrigatorio

--- script.js
+++ script.js
@@ -7,19 +7,22 @@ btnInserir.addEventListener('click', function (event) {
     const produto    = document.getElementById('produtoNome').value;
     const categoria  = document.getElementById('produtoCategoria').value;
     const quantidade = document.getElementById('produtoQuantidade').value;
+    
+    if (produto) {
+       listaProdutos.push( {
+           produto: produto,
+           categoria: categoria,
+           quantidade: quantidade
+       })
+    };
 
-    listaProdutos.push( {
-        produto: produto,
-        categoria: categoria,
-        quantidade: quantidade
-    })
 
    // console.log(listaProdutos)
     let htmlContent = '';
     const elementoListaProdutos = document.getElementById("listaProdutos");
 
     listaProdutos.forEach(produto => {
-        htmlContent += `<div>${produto.produto}, ${produto.categoria}, ${produto.quantidade}</div>`;
+        htmlContent += `<div>${produto.produto} ${produto.categoria} ${produto.quantidade}</div>`;
     });
 
     elementoListaProdutos.innerHTML = htmlContent;
