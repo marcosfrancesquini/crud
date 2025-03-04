8bdb2784f6f1da828c814ffa2b8fda55a72d2731
marcosfrancesquini <francesquini.personal@gmail.com>
2025-03-04 17:04:31 -0300

    ordenar implantado

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 listaProdutos = []
 
 const btnInserir = document.getElementById("inserir")
+const btnOrdenar = document.getElementById("ordenar")
 
-btnInserir.addEventListener('click', function (event) {
+btnInserir.addEventListener('click', function(event) {
     event.preventDefault();
     const produto    = document.getElementById('produtoNome').value;
     const categoria  = document.getElementById('produtoCategoria').value;
@@ -18,7 +19,7 @@ btnInserir.addEventListener('click', function (event) {
 
 
    // console.log(listaProdutos)
-    let htmlContent = '';
+    let htmlContent = "";
     const elementoListaProdutos = document.getElementById("listaProdutos");
 
     listaProdutos.forEach(produto => {
@@ -27,5 +28,29 @@ btnInserir.addEventListener('click', function (event) {
 
     elementoListaProdutos.innerHTML = htmlContent;
     
-
 });
+
+btnOrdenar.addEventListener("click", function(event){
+    event.preventDefault();
+    listaProdutos.sort((a, b) => {
+        if (a.categoria < b.categoria) {
+            return -1;
+        }
+        if (a.categoria > b.categoria) {
+            return 1;
+        }
+        return 0;
+    });
+    console.log(listaProdutos.categoria);
+    let ordemContent = "";
+    const elementoListaProdutos = document.getElementById("listaProdutos");
+    elementoListaProdutos.innerHTML = ""; // Limpa a lista antes de adicionar os itens
+
+    listaProdutos.forEach(produto => {
+        const li = document.createElement('li');
+        li.textContent = `${produto.produto} - ${produto.categoria} - ${produto.quantidade}`;
+        elementoListaProdutos.appendChild(li);
+    });
+    
+});
+
